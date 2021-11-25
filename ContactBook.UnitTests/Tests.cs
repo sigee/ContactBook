255843f67109f3de4cac8563939812a08171815b
Sigee <sigee15@gmail.com>
2021-11-25 11:43:29 +0100

    Add UnitTests project

---
+++ ContactBook.UnitTests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace ContactBook.UnitTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
